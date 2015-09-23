5ad028ca5948ef5167068eaac08e2560b2abeebf
Tobias Preuss <tobias.preuss@googlemail.com>
2015-09-23 15:45:01 +0200

    Enable settings for PostGIS.

--- styrax/settings_development.py
+++ styrax/settings_development.py
@@ -14,7 +14,7 @@ DEBUG = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': 'postgres',
     }
 }
