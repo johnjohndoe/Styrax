1952dd7d97651bb669ce4cbca562d833c7aa5854
Tobias Preuss <tobias.preuss@googlemail.com>
2015-10-11 00:46:55 +0200

    Allow int values only.

--- berlin/geoutils.py
+++ berlin/geoutils.py
@@ -6,7 +6,14 @@ from django.contrib.gis.measure import D
 
 
 def distance_between_trees(tree1, tree2):
-    return tree1.distance(tree2)
+    tree1_pk = int(tree1)
+    tree2_pk = int(tree2)
+    if isinstance(tree1_pk, int) and isinstance(tree2_pk, int):
+        real_tree1 = BerlinStreetTrees.objects.get(pk=tree1_pk)
+        real_tree2 = BerlinStreetTrees.objects.get(pk=tree2_pk)
+        return real_tree1.distance(real_tree2)
+    raise ValueError("Parameters must be of type 'int'. "
+                     "Check parameter: {0} and {1}".format(tree1, tree2))
 
 
 def trees_within_distance(origin, distance):
