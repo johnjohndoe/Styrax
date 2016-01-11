d47aacb225b025d4ea9880cb04ec6329588d94fd
Tobias Preuss <tobias.preuss@googlemail.com>
2016-01-11 10:42:46 +0100

    Respond with error when arguments are missing.

--- berlin/views.py
+++ berlin/views.py
@@ -22,12 +22,25 @@ class BerlinStreetTreeViewSet(viewsets.ModelViewSet):
 
 class DistanceView(viewsets.ViewSet):
 
-    """
-    API endpoint which returns the distance between two trees
-    """
     def list(self, request, format=None):
+        """
+        API endpoint which returns the distance between two trees
+        param: tree1 First tree
+        param: tree2 Second tree
+        """
         tree1 = request.query_params.get('tree1', None)
         tree2 = request.query_params.get('tree2', None)
+
+        if tree1 is None or tree2 is None:
+            response_data = {
+                'error': 'Missing argument(s).',
+                'required_arguments': [
+                    'tree1 [integer]',
+                    'tree2 [integer]'
+                ]
+            }
+            return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
+
         distance = geoutils.distance_between_trees(tree1, tree2)
         distance_hash = {'distance': distance}
         return Response(distance_hash, status=status.HTTP_200_OK)
